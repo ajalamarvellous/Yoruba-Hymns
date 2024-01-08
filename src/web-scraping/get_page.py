34a58f3ddbbba54bda0d3f8d5de6780766e39fa7
shelby <ajalaoluwamayowa00@gmail.com>
2024-01-08 18:12:46 +0100

    Add robust exception under retrieving the link incase one of the two is unavailable

--- src/web-scraping/get_page.py
+++ src/web-scraping/get_page.py
@@ -70,10 +70,19 @@ def extract_info(page: str, csv_writer: object):
     # save the content to a csv file
     csv_writer.writerow([soup.title, content_n])
     # get links to new pages to crawl also
-    next_link = soup.select(".elementor-post-navigation__next.elementor-post-navigation__link a")[0].get("href")
-    prev_link = soup.select(".elementor-post-navigation__prev.elementor-post-navigation__link a")[0].get("href")
-    logger.info(f"The new links to crawl {next_link, prev_link}")
-    return [prev_link, next_link]
+    returned_links =[]
+    try:
+        next_link = soup.select(".elementor-post-navigation__next.elementor-post-navigation__link a")[0].get("href")
+        returned_links.append(next_link)
+    except IndexError:
+        logger.error("Next link out of index")  
+    try:  
+        prev_link = soup.select(".elementor-post-navigation__prev.elementor-post-navigation__link a")[0].get("href")
+        returned_links.append(prev_link)
+    except IndexError:
+        logger.error("Previous link out of index")  
+    logger.info(f"The new links to crawl {returned_links}")
+    return returned_links
 
 
 def create_csv(file_address: str):
@@ -94,29 +103,34 @@ def update_list(returned_links: list, parsed_pages: list, unparsed_pages: list):
     parsed_pages.append(unparsed_pages[0])
     # delete the link from unparsed_pages list
     del unparsed_pages[0]
-    # for the links in the returned_list, check if they've not been parsed before
-    for link in returned_links:
-        if link not in parsed_pages:
-            # if they haven't, add to unparsed_list
-            unparsed_pages.append(link)
-    logger.info(f"Lists modified respectively... \n parsed_pages: {parsed_pages}, \nunparsed_pages: {unparsed_pages}")
+    # check if the returned_links list is not empty
+    if returned_links != []:
+        # for the links in the returned_list, check if they've not been parsed before
+        for link in returned_links:
+            if link not in parsed_pages:
+                # if they haven't, add to unparsed_list
+                unparsed_pages.append(link)
+        logger.info(f"Lists modified respectively... \n parsed_pages:   {parsed_pages}, \nunparsed_pages: {unparsed_pages}")
     return parsed_pages, unparsed_pages
 
 
 def main():
-    parsed_pages, unparsed_pages = [], []
-    unparsed_pages.append(configurations["url"])
-    file, csv_writer = create_csv(configurations["hymn_file"])
-    while unparsed_pages != []:
-        page = get_page(unparsed_pages[0])
-        # file = save_file(page.text)
-        returned_links = extract_info(page.content, csv_writer)
-        parsed_pages, unparsed_pages = update_list(
-                                            returned_links, 
-                                            parsed_pages, 
-                                            unparsed_pages
-                                            )
-    file.close()
+    try:
+        parsed_pages, unparsed_pages = [], []
+        unparsed_pages.append(configurations["url"])
+        file, csv_writer = create_csv(configurations["hymn_file"])
+        while unparsed_pages != []:
+            page = get_page(unparsed_pages[0])
+            # file = save_file(page.text)
+            returned_links = extract_info(page.content, csv_writer)
+            parsed_pages, unparsed_pages = update_list(
+                                                returned_links, 
+                                                parsed_pages, 
+                                                unparsed_pages
+                                                )
+        file.close()
+    except Exception as err:
+        print(f"An error occured: {err}")
 
 
 if __name__ == "__main__":
