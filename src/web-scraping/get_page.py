03ec53fc218fc2e25811253e2a7de8c71f1b99a9
shelby <ajalaoluwamayowa00@gmail.com>
2024-01-08 16:21:20 +0100

    Add update_list to automatically add and sort new pages to crawl

--- src/web-scraping/get_page.py
+++ src/web-scraping/get_page.py
@@ -51,7 +51,7 @@ def extract_info(page: str, csv_writer: object):
     # define soup object
     soup = BeautifulSoup(page, "html.parser")
     # get page title
-    print(f"Page title: {soup.title}")
+    logger.info(f"Page title: {soup.title}")
     # get the content of the page
     content = soup.select(".elementor-widget-container p")
     # recontruct the content
@@ -62,14 +62,15 @@ def extract_info(page: str, csv_writer: object):
         # redundant values that do not include the hymns we want 
         if (i > 0) and (i < len(content) - 2):
             content_n += f"{p} "
-    print(f"Page content {content_n} \n\n")
+    # print(f"Page content {content_n} \n\n")
 
     # save the content to a csv file
     csv_writer.writerow([soup.title, content_n])
     # get links to new pages to crawl also
-    next_link = soup.select(".elementor-post-navigation__next.elementor-post-navigation__link a")
-    prev_link = soup.select(".elementor-post-navigation__prev.elementor-post-navigation__link a")[0]["href"]
-    print(f"The new links to crawl {next_link[0].get("href"), prev_link}")
+    next_link = soup.select(".elementor-post-navigation__next.elementor-post-navigation__link a")[0].get("href")
+    prev_link = soup.select(".elementor-post-navigation__prev.elementor-post-navigation__link a")[0].get("href")
+    logger.info(f"The new links to crawl {next_link, prev_link}")
+    return [prev_link, next_link]
 
 
 def create_csv(file_address: str):
@@ -80,9 +81,40 @@ def create_csv(file_address: str):
     return file, csv_writer
 
 
-if __name__ == "__main__":
-    page = get_page(configurations["url"])
+def update_list(returned_links: list, parsed_pages: list, unparsed_pages: list):
+    """
+    Update the different lists by removing parsed page from the unparsed_pages list 
+    and adding the new returned list to unparsed_pages list given that they have not 
+    been parsed before 
+    """
+    # add the first link on the unparsed_pages to the list of parsed_pages
+    parsed_pages.append(unparsed_pages[0])
+    # delete the link from unparsed_pages list
+    del unparsed_pages[0]
+    # for the links in the returned_list, check if they've not been parsed before
+    for link in returned_links:
+        if link not in parsed_pages:
+            # if they haven't, add to unparsed_list
+            unparsed_pages.append(link)
+    logger.info(f"Lists modified respectively... \n parsed_pages: {parsed_pages}, \nunparsed_pages: {unparsed_pages}")
+    return parsed_pages, unparsed_pages
+
+
+def main():
+    parsed_pages, unparsed_pages = [], []
+    unparsed_pages.append(configurations["url"])
     file, csv_writer = create_csv(configurations["hymn_file"])
-    # file = save_file(page.text)
-    extract_info(page.content, csv_writer)
-    print(file.name)
+    while unparsed_pages != []:
+        page = get_page(unparsed_pages[0])
+        # file = save_file(page.text)
+        returned_links = extract_info(page.content, csv_writer)
+        parsed_pages, unparsed_pages = update_list(
+                                            returned_links, 
+                                            parsed_pages, 
+                                            unparsed_pages
+                                            )
+    file.close()
+
+
+if __name__ == "__main__":
+    main()
