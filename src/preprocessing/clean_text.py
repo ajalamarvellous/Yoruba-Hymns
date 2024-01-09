7cda70de08716ff9777378b5dd7a312a1cb6ddd7
shelby <ajalaoluwamayowa00@gmail.com>
2024-01-09 19:38:54 +0100

    Read file and add header

--- src/preprocessing/clean_text.py
+++ src/preprocessing/clean_text.py
@@ -2,6 +2,8 @@ import logging
 import yaml
 from pathlib import Path
 
+import pandas as pd
+
 
 logging.basicConfig(
     format= "%(asctime)s [%(levelname)s] \
@@ -14,3 +16,12 @@ logger = logging.getLogger()
 config_file_path = Path(__file__).parents[2]/ "config.yml"
 with open(config_file_path) as config_file:
     configurations = yaml.safe_load(config_file)
+
+
+def read_file(file_address: str) -> pd.DataFrame:
+    """
+    Read file and add headers
+    """
+    df = pd.read_csv(file_address, names=['Title', 'Hymn'])
+    logger.info("Dataframe read successfully...")
+    return df
